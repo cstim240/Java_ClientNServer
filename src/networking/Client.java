ab2577c60e0360b1a595378202c919aa75bf13b8
Tim Supan <timsupann@gmail.com>
2023-04-02 20:07:03 -0700

    First commit

---
+++ src/networking/Client.java
@@ -0,0 +1,25 @@
+package networking;
+import java.io.*;
+import java.net.Socket;
+
+/** <h1> Lab 7 - Java Networking - Client and Server </h1>
+ * <p>
+ * Description: Modify the networking classes to enable server to computer letter grade 
+ * and have the server display its communication with clients.
+ * The Client should be able to send any grade between 0 and 100, disconnect using -1, 
+ * and display its communication with the server.
+ * </p>
+ * <h2> CPSC 1181 - 002 </h2>
+ * @author Tim Supan
+ * @see <a href="https://docs.oracle.com/en/java/"> Java Documentation </a> 
+ * @version 18.0.1.1
+ * @since 2023-04-04
+ */
+
+public class Client {
+
+	public static void main(String[] args) {
+
+	}
+
+}
